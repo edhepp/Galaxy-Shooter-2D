a4e7bc5b7af4012a242f5a6a32cf2efce4d47f0b
unknown <ed.hepplewhite@gmail.com>
2022-06-21 20:27:07 -0500

    Added assets and background

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,13 +13,19 @@ public class Player : MonoBehaviour
     private float _canFire = -1.0f;
     [SerializeField]
     private int _lives = 3;
+    private SpawnManager _spawnManager;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        // position the player at 0, 0, 0 when the game starts
         transform.position = new Vector3(0, 0, 0);
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -73,7 +79,8 @@ public class Player : MonoBehaviour
         _lives -= 1;
 
         if (_lives < 1)
-        { 
+        {
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
     }
